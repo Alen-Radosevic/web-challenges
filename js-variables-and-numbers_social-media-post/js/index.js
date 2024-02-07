0e8003219f7bd9fd176a6cf8022d70748ebc1a6b
Alen Radosevic <alenradosevic@Alens-Air.Speedport_W723_V_Typ_A_1_01_022>
2024-02-07 11:46:28 +0100

    Add new const.

---
+++ js-variables-and-numbers_social-media-post/js/index.js
@@ -0,0 +1,31 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+
+// --v-- write your code here --v--
+const title = "Social Media Post";
+const text = "Welcome to this exercise";
+const textParagraph = "Please open the console before you start.";
+let numberLikes = 50;
+const user = "Alen";
+const isReported = "true";
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+
+// --v-- write your code here --v--
+console.log(title);
+console.log(text);
+console.log(numberLikes++);
+console.log(user);
+console.log(isReported);
+// --^-- write your code here --^--
