bff47ec222810a42a34a2917131701ca2ee16462
Alen Radosevic <alenradosevic@Alens-Air.Speedport_W723_V_Typ_A_1_01_022>
2024-02-12 22:29:15 +0100

    updade2

--- js-inputs-and-strings_switch-values/js/index.js
+++ js-inputs-and-strings_switch-values/js/index.js
@@ -14,13 +14,26 @@ Hint: the HTML entity for the switch button is: &updownarrow
 */
 
 const firstInput = document.querySelector('[data-js="first-input"]');
+
+const secondInput = document.querySelector("#second-input");
+
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const switchButton = document.querySelector('[data-js="button-switch"]');
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
+  secondInput.value = secondInput.value.toUpperCase();
 });
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+  secondInput.value = secondInput.value.toLowerCase();
+});
+
+switchButton.addEventListener("click", () => {
+  const firstInputStartValue = firstInput.value;
+  const secondInputStartValue = secondInput.value;
+  firstInput.value = secondInputStartValue;
+  secondInput.value = firstInputStartValue;
 });
