193c2a79ec147b96a050ec7d1070e5c558933cd1
Alen Radosevic <alenradosevic@Alens-Air.Speedport_W723_V_Typ_A_1_01_022>
2024-02-15 16:02:37 +0100

    add all commits

---
+++ js-objects-and-arrays_objects/index.js
@@ -0,0 +1,48 @@
+console.clear();
+
+// EXERCISE 1
+// Create an object personWithNameAgeEmail with the properties "name", "age" and "email".
+
+const personWithNameAgeEmail = {
+  example: "example",
+  name: "Mark",
+  age: 38,
+  email: marko.email.com,
+};
+
+// EXERCISE 2
+// Set the values of name and age equal to the corresponding values of the personAlex object.
+
+const personAlex = {
+  name: "Alex",
+  age: 24,
+};
+
+const nameOfAlex = personAlex[0];
+const ageOfAlex = personAlex[1];
+
+console.log(nameOfAlex);
+
+// EXERCISE 3
+// Change the value of name to "Alex" and the value of age to "35" by updating the object personToChange: person.existingProperty = newValue.
+
+const personToChange = {
+  name: "Kim",
+  age: 24,
+};
+
+// EXERCISE 4
+// Add the properties age "5" and breed "husky" to the object petPluto: pet.newProperty = newValue.
+
+const petPluto = {
+  name: "Pluto",
+  species: "dog",
+};
+
+export {
+  personWithNameAgeEmail,
+  nameOfAlex,
+  ageOfAlex,
+  personToChange,
+  petPluto,
+};
