b61d4b39c5b2a388c28e54e5443128c5f093d8b3
Alen Radosevic <alenradosevic@Alens-Air.Speedport_W723_V_Typ_A_1_01_022>
2024-02-18 18:47:15 +0100

    update

---
+++ js-structure_shapes/index.js
@@ -0,0 +1,7 @@
+import Circle from "./components/Circle/Circle.js";
+import Square from "./components/Square/Square.js";
+import Pentagon from "./components/Pentagon/Pentagon.js";
+
+const root = document.getElementById("root");
+
+root.append(Circle, Square, Pentagon);
