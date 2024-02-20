92b679506091fab19961dc74ee464e8728722601
Alen Radosevic <alenradosevic@Alens-Air.Speedport_W723_V_Typ_A_1_01_022>
2024-02-20 09:17:22 +0100

    console

--- js-fetch_star-wars-console/js/index.js
+++ js-fetch_star-wars-console/js/index.js
@@ -2,8 +2,10 @@ console.clear();
 
 async function fetchData() {
   const response = await fetch("https://swapi.dev/api/people");
-  const data = await response.json();
-  return data;
+  const json = await response.json();
+  console.log(json.results[2].eye_color);
+  const foundCharacter = json.results.find(({ name }) => name == "R2-D2");
+  console.log(foundCharacter);
 }
 
 fetchData();
