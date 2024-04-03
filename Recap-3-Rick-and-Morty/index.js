20bef808a85b00d016666aa33c6ec8213f7ce6cd
Alen Radosevic <alenradosevic@Alens-Air.Speedport_W723_V_Typ_A_1_01_022>
2024-04-03 18:55:42 +0200

    alen-changes

---
+++ Recap-3-Rick-and-Morty/index.js
@@ -0,0 +1,33 @@
+import CharacterCard from "./components/CharacterCard/CharacterCard.js";
+CharacterCard();
+const cardContainer = document.querySelector('[data-js="card-container"]');
+const searchBarContainer = document.querySelector(
+  '[data-js="search-bar-container"]'
+);
+const searchBar = document.querySelector('[data-js="search-bar"]');
+const navigation = document.querySelector('[data-js="navigation"]');
+const prevButton = document.querySelector('[data-js="button-prev"]');
+const nextButton = document.querySelector('[data-js="button-next"]');
+const pagination = document.querySelector('[data-js="pagination"]');
+
+// States
+const maxPage = 1;
+const page = 1;
+const searchQuery = "";
+
+async function fetchCharacters() {
+  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const data = await response.json();
+  console.log(data);
+  return data.results;
+}
+
+fetchDataAndRender();
+async function fetchDataAndRender() {
+  const serienCharacters = await fetchCharacters();
+  console.log(serienCharacters);
+  serienCharacters.forEach((character) => {
+    const newCard = CharacterCard();
+    cardContainer.append(newCard);
+  });
+}
