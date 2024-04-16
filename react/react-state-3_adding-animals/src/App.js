5f6ab9a51bcccd5ec3ec69cdb277de1d2571f77d
Alen Radosevic <alenradosevic@Alens-Air.Speedport_W723_V_Typ_A_1_01_022>
2024-04-16 12:18:07 +0200

    add_35

--- react/react-state-3_adding-animals/src/App.js
+++ react/react-state-3_adding-animals/src/App.js
@@ -32,7 +32,7 @@ export default function App() {
       { name: newAnimal.name, emoji: newAnimal.emoji, id: uid() },
     ]);
 
-    setAnimals([...animals, { id: uid(), ...newAnimal }]);
+    // setAnimals([...animals, { id: uid(), ...newAnimal }]);
   }
 
   return (
