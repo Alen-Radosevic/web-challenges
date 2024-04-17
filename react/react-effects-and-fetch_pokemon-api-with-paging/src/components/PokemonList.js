a264d5ed671f9ecbc814ff18c5a1214ed32c7572
Alen Radosevic <alenradosevic@Alens-Air.Speedport_W723_V_Typ_A_1_01_022>
2024-04-17 12:00:36 +0200

    paging

---
+++ react/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+export default function PokemonList() {
+  const [pokemon, setPokemon] = useState([]);
+  const [page, setPage] = useState(0);
+
+  function handlePreviousButton() {
+    setPage(page + 20);
+  }
+
+  function handleNextPage() {
+    setPage(page - 20);
+  }
+
+  useEffect(() => {
+    async function loadPokemon() {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon?offset=${page}`
+        );
+        const data = await response.json();
+        setPokemon(data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    loadPokemon();
+  }, [page]);
+
+  return (
+    <main>
+      <button type="button" onClick={handlePreviousButton}>
+        Previous Page
+      </button>
+      <button type="button" onClick={handleNextPage}>
+        Next Page
+      </button>
+      <ul>
+        {pokemon.map(({ name }) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+    </main>
+  );
+}
