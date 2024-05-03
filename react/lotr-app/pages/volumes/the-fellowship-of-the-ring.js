14bdce50b803b4f2d0973cf8a1051c3b3d7ad0a6
Alen Radosevic <alenradosevic@Alens-MacBook-Air.local>
2024-05-03 09:14:27 +0200

    lotr-app

---
+++ react/lotr-app/pages/volumes/the-fellowship-of-the-ring.js
@@ -0,0 +1,31 @@
+import { volumes } from "@/lib/data";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Volumes1() {
+  const dataVolume1 = volumes.find(
+    ({ slug }) => slug === "the-fellowship-of-the-ring"
+  );
+
+  return (
+    <>
+      <Link href="../">← All Volumes</Link>
+      <h1>{dataVolume1.title}</h1>
+      <p>{dataVolume1.description}</p>
+      <ul>
+        <li>
+          {dataVolume1.books[0].ordinal}: {dataVolume1.books[0].title}
+        </li>
+        <li>
+          {dataVolume1.books[1].ordinal}: {dataVolume1.books[1].title}
+        </li>
+      </ul>
+      <Image
+        src="/images/the-fellowship-of-the-ring.png"
+        height={230}
+        width={140}
+        alt="the bookcover of the fellowship of the ring"
+      ></Image>
+    </>
+  );
+}
