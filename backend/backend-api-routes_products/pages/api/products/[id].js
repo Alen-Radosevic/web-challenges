701d686680a969cd76cef01b459e4483b8ab1025
Alen Radosevic <a.aradosevic87@gmail.com>
2024-05-31 13:53:31 +0200

    done

--- backend/backend-api-routes_products/pages/api/products/[id].js
+++ backend/backend-api-routes_products/pages/api/products/[id].js
@@ -2,6 +2,10 @@ import { getProductById } from "@/services/productServices";
 
 export default function handler(request, response) {
   const { id } = request.query;
+  const product = getProductById(id);
 
+  if (!product) {
+    response.status(2404).json({ status: "Is not exist" });
+  }
   response.status(200).json(getProductById(id));
 }
--- backend/backend-api-routes_products/pages/products/[id].js
+++ backend/backend-api-routes_products/pages/products/[id].js
@@ -1,23 +1,21 @@
 import { useRouter } from "next/router";
 import useSWR from "swr";
 
-// const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function DetailPage() {
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, error, isLoading } = useSWR("/api/products/", fetcher);
+  const { data, error, isLoading } = useSWR(`/api/products/${id}`, fetcher);
 
   if (error) return <div>Error</div>;
   if (isLoading) return <div>Loading...</div>;
 
-  const productData = data.find((product) => product.id === id);
-
   return (
-    <section>
-      <h2>{productData.name}</h2>
-      <p>{productData.description}</p>
-    </section>
+    <div>
+      <h2>{data.name}</h2>
+      <p>{data.description}</p>
+    </div>
   );
 }
