46637b702414a6c227245dfee604e8c52b089702
Alen Radosevic <a.aradosevic87@gmail.com>
2024-06-05 07:02:59 +0200

    backend-read_product

---
+++ backend/backend-read_products/pages/api/products/[id].js
@@ -0,0 +1,20 @@
+// import { products } from "../../../lib/products";
+import dbConnect from "@/db/connect";
+import Product from "@/db/models/Product";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  const { id } = request.query;
+
+  // const product = products.find((product) => product._id === id);
+
+  // if (!request.method === "GET") {
+  const product = await Product.findById(id).populate("reviews");
+
+  if (!product) {
+    return response.status(404).json({ status: "Not Found" });
+  }
+
+  return response.status(200).json(product);
+  // }
+}
